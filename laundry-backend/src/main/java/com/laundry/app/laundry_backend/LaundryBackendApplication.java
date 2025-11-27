f8c59c45010c244b60737dd01f9764d35c92370a
Adam Selmane <adam.selmane@epita.fr>
2025-11-27 12:30:48 +0100

    Spring boot integration

---
+++ laundry-backend/src/main/java/com/laundry/app/laundry_backend/LaundryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laundry.app.laundry_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryBackendApplication.class, args);
+	}
+
+}
